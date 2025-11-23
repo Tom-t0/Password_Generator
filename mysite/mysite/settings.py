d01218a473a750d28ea8e59f8f1ca9a29ed591e5
tomo <tomohitotakada@outlook.com>
2025-11-22 19:32:28 -0500

    final

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l12o#gvse#zasqveo^+c+ug!k+gk!%^c7!3bus_^9e(746!0)!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "api",
 ]
 
 MIDDLEWARE = [
